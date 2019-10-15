e0b39a8fe2c191ce4816d3de5f46d6943280e463
bradley <bradley.m.morton@gmail.com>
2019-10-15 14:41:15 -0800

    added compiling file. It has a dummy return value and does not work

---
+++ HW_03/contigsum.hpp
@@ -0,0 +1,36 @@
+//Contiguous sums by Bradley Morton
+//Created for Dr. Hartman's CS411 (Analysis of Algorithms) course
+//Finds largest continuous sum with divide and conquer algorithm
+
+#ifndef CONTIGSUM_HPP
+#define CONTIGSUM_HPP
+
+
+template<typename RAIter>
+int contigSum(RAIter first, RAIter last)
+{
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
